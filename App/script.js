3c17bdf85dc23a968ff2bedb89c58ada77b4a84f
Cedram <bradar748@gmail.com>
2023-11-24 15:55:25 +0300

    Initial commit

---
+++ App/script.js
@@ -0,0 +1,51 @@
+const dataExample = [
+  {
+    company: "Bayerische Motoren Werke AG",
+    shef: "Angela Merkel",
+    country: "Germany",
+  },
+  {
+    company: "Grupo Bimbo S.A.B. de C.V.",
+    shef: "Enrique Peña Nieto",
+    country: "Mexico",
+  },
+  {
+    company: "Red Bull GmbH",
+    shef: "Sebastian Kurz",
+    country: "Austria",
+  },
+  {
+    company: "Vodafone Group plc",
+    shef: "Boris Johnson",
+    country: "United Kingdom",
+  },
+  {
+    company: "Toyota Motor Corporation",
+    shef: "Shinzo Abe",
+    country: "Japan",
+  },
+];
+
+let gridView = new GridView();
+gridView.header = "Hello";
+gridView.headerClass = ["header", "site-header"];
+gridView.attribute = {
+  company: {
+    label: "Компания",
+    src: "html",
+  },
+  chef: {
+    label: "Директор",
+  },
+  country: {
+    label: "Страна",
+    value: (data) => {
+      if (data["country"] === "Germany") {
+        return data["country"] + " map";
+      }
+      return data["country"];
+    },
+  },
+};
+gridView.data = dataExample;
+gridView.render();
